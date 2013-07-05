985b9f49b2ea353af3245d937a77c25920233f5d
shanegray <shane.joseph.gray@gmail.com>
2013-07-05 12:08:13 +0100

    add test projects

---
+++ CQRS/CQRS.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using CQRS.Web.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Web.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Get_SaysHello()
+        {
+            var sut = new HomeController();
+            Assert.AreEqual("CQRS API - Hello World", sut.Get());
+        }
+    }
+}
